467b06413bed71d211b34fb19577775274e1da9d
stuwilmur <stuwilmur@users.noreply.github.com>
2024-07-21 16:47:26 +0100

    Fix final health test

--- test/model/models/health.test.js
+++ test/model/models/health.test.js
@@ -1,12 +1,12 @@
 import * as model from '../../../src/model';
 import * as data from './data/health';
+import * as health from '../../../src/interface';
 
 test(`Tests coverage for 10% increase in GRPC, no governance adjustment,
  for a single country and year`, () => {
-  const result = model.models.governance
-    .createGovernanceConstantAdjustmentModel(0)
-    .add(model.models.revenue.createGrpcFromPercentageIncreaseModel())
-    .add(model.models.coverage.createCoverageModel())
-    .data(data.input);
+  const result = health.instantaneous(
+    {grpcValue: 10, grpcMethod: health.GrpcMethod.PERCENTAGE_INCREASE},
+    data.input,
+  );
   expect(result).toStrictEqual(data.expectedResult);
 });
